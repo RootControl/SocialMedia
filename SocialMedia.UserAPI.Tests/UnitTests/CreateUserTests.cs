a77fd22602cdf054ef60b4e9fdd93d2ef5591453
RootControl <lh.root.control@gmail.com>
2025-01-26 17:10:29 -0300

    Created new Test project for UserAPI and created tests for creating new user.

---
+++ SocialMedia.UserAPI.Tests/UnitTests/CreateUserTests.cs
@@ -0,0 +1,55 @@
+﻿using SocialMedia.UserAPI.Requests;
+using SocialMedia.UserAPI.Services;
+
+namespace SocialMedia.UserAPI.Tests.UnitTests;
+
+public class CreateUserTests
+{
+    private readonly VerifySettings _verifySettings;
+
+    public CreateUserTests()
+    {
+        _verifySettings = new VerifySettings();
+        _verifySettings.UseDirectory("snapshots");
+    }
+        
+    [Fact]
+    public Task CreateUserAsync_ShouldReturnError_WhenValidatingRequest()
+    {
+        var request = new CreateUserRequest();
+        var service = new CreateUserService();
+        
+        var response = service.CreateUserAsync(request);
+        
+        return Verify(response, _verifySettings);
+    }
+    
+    [Fact]
+    public Task CreateUserAsync_ShouldReturnError_WhenRequestIsInvalid()
+    {
+        var request = new CreateUserRequest
+        {
+            Username = "johndoe"
+        };
+        var service = new CreateUserService();
+        
+        var response = service.CreateUserAsync(request);
+        
+        return Verify(response, _verifySettings);
+    }
+    
+    [Fact]
+    public Task CreateUserAsync_ShouldReturnSuccess_WhenRequestIsValid()
+    {
+        var request = new CreateUserRequest
+        {
+            Username = "johndoe",
+            DisplayName = "John Doe"
+        };
+        var service = new CreateUserService();
+        
+        var response = service.CreateUserAsync(request);
+        
+        return Verify(response, _verifySettings);
+    }
+}
---
+++ SocialMedia.UserAPI.Tests/VerifyChecksTest.cs
@@ -0,0 +1,8 @@
+namespace SocialMedia.UserAPI.Tests;
+
+public class VerifyChecksTest
+{
+    [Fact]
+    public Task Run() =>
+        VerifyChecks.Run();
+}
